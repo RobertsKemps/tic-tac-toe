989968842ab56b16b5e603376931c799771580d3
RobertsKemps <robertskemps@gmail.com>
2022-09-26 20:59:17 +0300

    Formating

--- resources/js/Services/aiLogicService.js
+++ resources/js/Services/aiLogicService.js
@@ -1,6 +1,6 @@
+//Minimax algorithm is not quite working as it should
 export const minimax = (board, player, depth = 1) => {
-    // The 'o' player wants to maximize its score, the 'x' player wants to
-    // minimize its score.
+    // The 'o' player wants to maximize its score, the 'x' player wants to minimize its score.
     let bestScore = player === 'O' ? -10000 : 10000;
     let bestMove = null;
     let moves = getPossibleMoves(board);
@@ -11,8 +11,7 @@ export const minimax = (board, player, depth = 1) => {
         makeAiMove(move.x, move.y, player, newBoard);
         // Recursively call the minimax function for the new board.
         const score = minimax(newBoard, player === 'X' ? 'O' : 'X', depth + 1).score;
-        // If the score is better than the best saved score update the best saved
-        // score to this move.
+        // If the score is better than the best saved score update the best saved score to this move.
         if ((player === 'O' && score > bestScore) || (player === 'X' && score < bestScore)) {
             bestScore = score;
             bestMove = move;
